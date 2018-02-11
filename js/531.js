1b0fe6b3af8df65695f827f829716d46b552336e
radioactivesandwich <bpg62983@gmail.com>
2018-02-11 17:59:46 -0600

    Added code to get and replace inner HTML. Pulling 1rm values and replacing with max.

--- js/531.js
+++ js/531.js
@@ -1,1 +1,14 @@
 /* JS Page */
+
+window.onload = function () {
+
+var element = document.getElementById("rm1").innerHTML;
+
+document.getElementById("max1").innerHTML = element;
+
+console.log(element);
+
+}
+
+
+
