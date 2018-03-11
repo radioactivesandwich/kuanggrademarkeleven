11ad40b1f83296826097f315649efed62122755f
radioactivesandwich <bpg62983@gmail.com>
2018-03-11 17:18:57 -0500

    Created Week 1 schedule manually in HTML

--- js/531.js
+++ js/531.js
@@ -2,11 +2,15 @@
 
 window.onload = function () {
 
-var element = document.getElementById("rm1").innerHTML;
+var element1 = document.getElementById("rm1").innerHTML * 0.9;
+var element2 = document.getElementById("rm2").innerHTML * 0.9;
+var element3 = document.getElementById("rm3").innerHTML * 0.9;
+var element4 = document.getElementById("rm4").innerHTML * 0.9;
 
-document.getElementById("max1").innerHTML = element;
-
-console.log(element);
+document.getElementById("max1").innerHTML = element1;
+document.getElementById("max2").innerHTML = element2;
+document.getElementById("max3").innerHTML = element3;
+document.getElementById("max4").innerHTML = element4;
 
 }
 
