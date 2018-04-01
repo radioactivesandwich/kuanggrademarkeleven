73ae9fa32425bb4370211b8f88c83727eff12679
radioactivesandwich <bpg62983@gmail.com>
2018-04-01 14:45:39 -0500

    Added week 2 to 531 webpage

--- js/531.js
+++ js/531.js
@@ -2,15 +2,20 @@
 
 window.onload = function () {
 
+//Tax 1RM figures and multiply by 90%
 var element1 = document.getElementById("rm1").innerHTML * 0.9;
 var element2 = document.getElementById("rm2").innerHTML * 0.9;
 var element3 = document.getElementById("rm3").innerHTML * 0.9;
 var element4 = document.getElementById("rm4").innerHTML * 0.9;
 
+//Take the 90% 1RM figure and input it in the Max cells
 document.getElementById("max1").innerHTML = element1;
 document.getElementById("max2").innerHTML = element2;
 document.getElementById("max3").innerHTML = element3;
 document.getElementById("max4").innerHTML = element4;
+
+//
+
 
 }
 
